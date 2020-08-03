be32335c35f54f5fc83eb48d395197bfc2f34874
Facundo Tenuta <facuargush@gmail.com>
2020-08-03 02:42:53 -0300

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'  //rafc snipet
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = props => {
+
+    const [categories, setCategories] = useState(["One Punch"]);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories}/>
+            <hr/>
+
+            
+
+            <ol>
+                {
+                    categories.map(category => (
+                        <GifGrid 
+                            key={category}
+                            category={category}
+                        />
+                    ))
+                }
+            </ol>
+
+        </>
+    )
+}
+
+
+export default GifExpertApp
