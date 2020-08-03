4a2ad28618c849ccc9d92ee326b90a7d95d6df71
Facundo Tenuta <facuargush@gmail.com>
2020-08-03 02:44:07 -0300

    docs agregado

---
+++ docs/precache-manifest.35e23ba3938cf9ccc4822887bf86b83a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfcc9d52e7dab202db2c2fa4802df782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eb52053832556daf386",
+    "url": "/static/css/main.e13a6c2e.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb52053832556daf386",
+    "url": "/static/js/main.28eba05c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
